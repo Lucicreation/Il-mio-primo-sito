3009781170be8edd7347c9a0e2927005c6aa11a8
Lucicreation <lfgd2024@gmail.com>
2024-12-19 18:37:59 +0000

    preghiamo

--- primo progetto/prova.js
+++ primo progetto/prova.js
@@ -1,1 +1,9 @@
-var pippo=10;
+function primafunzione() {
+    alert("Grazie per esserti unita a noi!");
+  }
+  function secondafunzione() {
+    alert("Lasciati guidare verso il tuo futuro!");
+  }
+  document.getElementById("changeStyles").addEventListener("click" , function () {
+  //Cambia il background di tutti gli h2
+  const allH2 = 
